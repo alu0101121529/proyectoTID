f8edd0afc68dd08e22dec117a40ae57ae0e1299a
alu0101133201 <50806571+alu0101133201@users.noreply.github.com>
2020-04-30 19:54:30 +0100

    Update pregunta_inicial.r

--- pregunta_inicial.r
+++ pregunta_inicial.r
@@ -29,3 +29,54 @@ conjuntacion <- subset(conjuntacion, select = -is_goal)
 View(conjuntacion)
 conjuntacion <- sqldf('SELECT distinct * from conjuntacion')
 View(conjuntacion)
+
+
+// ---------------------------------------------------------------------------
+install.packages("sqldf")
+install.packages("gsubfn")
+install.packages("proto")
+install.packages("RSQLite")
+install.packages("ggplot2")
+
+library(sqldf)
+library(gsubfn)
+library(proto)
+library(RSQLite)
+library(ggplot2)
+
+
+# Eliminamos columnas innecesarias y cambiamos identificadores a numéricosevents <- subset(events,select = -text)
+events <- subset(events,select = -id_event)
+events$eventID <- 1:nrow(events)
+events$matchID <- as.numeric(factor(events$id_odsp, levels=unique(events$id_odsp)))
+events <- subset(events, select = -id_odsp)
+
+ginf$matchID <- as.numeric(factor(ginf$id_odsp, levels=unique(ginf$id_odsp)))
+ginf <- subset(ginf, select = -id_odsp)
+ginf <- subset(ginf, select = -link_odsp)
+
+# Combinamos las tablas events y ginf
+mergeCols <- c("matchID", "matchID")
+inner <- merge(ginf, events, by = mergeCols)
+
+# Eliminamos filas que no tengan información detallada
+inner <- subset(inner, adv_stats == TRUE)
+inner <- subset(inner, select = -adv_stats)
+inner <- subset(inner, select = -c(odd_over, odd_under, odd_bts, odd_bts_n))
+
+# Creamos una nueva columna con el equipo ganador
+inner$winner <- ifelse(inner$fthg > inner$ftag, inner$ht, ifelse(inner$fthg < inner$ftag, inner$at, "tie"))
+
+# Añadimos el tipo de evento como una string
+
+inner$events_name <- factor(inner$event_type, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 
+       labels = c("Attempt(shot)", "Corner", "Foul", "YellowCard", 
+                  "SecondYellowCard", "(straight)RedCard", "Substitution", 
+                  "FreeKickWon", "Offside", "HandBall", "Penalty"))
+
+# Gráficas
+p <- ggplot(inner, aes(x=events_name)) + geom_bar()
+
+
+
+
